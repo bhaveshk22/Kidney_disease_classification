5912ad100922418d39792481d03018f0708e1f87
Bhavesh <bhaveshk2210@gmail.com>
2024-07-19 00:57:00 +0530

    model evaluation added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline 
 from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
-
+from cnnClassifier.pipeline.stage_04_model_evaluation_with_mlflow import ModelEvaluationPipeline
 
 STAGE_NAME = 'Data Ingestion Stage'
 try:
@@ -18,20 +18,31 @@ except Exception as e:
 STAGE_NAME = 'Prepare base model'
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    data_ingestion = PrepareBaseModelTrainingPipeline()
-    data_ingestion.main()
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e 
 
 
-STAGE_NAME = 'Model Training'
+STAGE_NAME = 'Training Stage'
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    data_ingestion = ModelTrainingPipeline()
-    data_ingestion.main()
+    model_training = ModelTrainingPipeline()
+    model_training.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e 
+
+
+STAGE_NAME = 'Evaluation Stage'
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_evaluation = ModelEvaluationPipeline()
+    model_evaluation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/cnnClassifier/pipeline/stage_04_model_evaluation_with_mlflow.py
+++ src/cnnClassifier/pipeline/stage_04_model_evaluation_with_mlflow.py
@@ -1,24 +1,23 @@
 from cnnClassifier.config.configuration import ConfigurationManager
-from cnnClassifier.components.model_training import Training
+from cnnClassifier.components.model_evaluation_with_mlflow import Evaluation
 from cnnClassifier import logger
 
 
 
-STAGE_NAME = "Training Stage"
+STAGE_NAME = "Evaluation Stage"
 
 
 
-class ModelTrainingPipeline:
+class ModelEvaluationPipeline:
     def __init__(self):
         pass
 
     def main(self):
         config = ConfigurationManager()
-        training_config = config.get_training_config()
-        training = Training(config=training_config)
-        training.get_base_model()
-        training.train_valid_generator()
-        training.train()
+        eval_config = config.get_evaluation_config()
+        evaluation = Evaluation(eval_config)
+        evaluation.evaluation()
+        evaluation.log_into_mlflow()
 
 
 
@@ -26,7 +25,7 @@ if __name__ == '__main__':
     try:
         logger.info(f"*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = ModelTrainingPipeline()
+        obj = ModelEvaluationPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
